3c544ef108e33b35afdcd7be15ac845aee209976
shahzoorkhan <lamborghinia693@gmail.com>
2023-05-18 19:17:29 +0500

    first commit

--- morvin/settings.py
+++ morvin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-()(=*r-un(n3kppp*h-)o(z8c2j9)73sfm7v-=7o75f*sj$!i2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Application definition
