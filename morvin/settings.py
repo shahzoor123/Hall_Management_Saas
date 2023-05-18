478c86c492e87307d623b04321fdd69355da6922
shahzoorkhan <lamborghinia693@gmail.com>
2023-05-18 21:50:17 +0500

    first commit

--- morvin/settings.py
+++ morvin/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import dj_database_url
+from decouple import config
 
 
 # Quick-start development settings - unsuitable for production
@@ -94,6 +96,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
