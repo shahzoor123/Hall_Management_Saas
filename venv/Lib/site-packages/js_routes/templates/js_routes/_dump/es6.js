4e4b0a7712dae717e0efc4ff38ac240b2477361a
shahzoorkhan <lamborghinia693@gmail.com>
2023-06-02 02:02:07 +0500

    try3

--- venv/Lib/site-packages/js_routes/templates/js_routes/_dump/es6.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-
-const routes = {{ routes|safe }};
-{% include "js_routes/_base/resolver.js" with var_assignment='const reverseUrl ' routes_var='routes' %}
-
-export default reverseUrl;
