b5bae9a81196770dc991a37f7203abcc6281d303
shahzoorkhan <lamborghinia693@gmail.com>
2023-06-21 14:18:21 +0500

    working on calender

--- morvin/views.py
+++ morvin/views.py
@@ -2,11 +2,23 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from allauth.account.views import PasswordSetView,PasswordChangeView
 from django.urls import reverse_lazy
+from ecommerce.models import EventSale
+from django.shortcuts import render
+
 
 class Index(LoginRequiredMixin,TemplateView):
     template_name = "index.html"
 class Calendar(LoginRequiredMixin,TemplateView):
     template_name = "calendar.html"
+
+    def get(self, request):
+        sale = EventSale.objects.all()
+        context = {
+            "sales": sale
+        }
+        return render(request, self.template_name, context)
+    
+
 class Chat(LoginRequiredMixin,TemplateView):
     template_name = "chat.html"
 class MyPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
