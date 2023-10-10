7421ac4d303d8efed36f5f677d747731e4086ded
Habib Ur Rehman <habib.rehman0088@gmail.com>
2023-10-10 17:47:53 +0500

    delete feat, and G.E query added

--- morvin/views.py
+++ morvin/views.py
@@ -10,7 +10,8 @@ from datetime import datetime
 from django.db.models import Count
 from django.shortcuts import render
 from ecommerce.models import Event
-
+from generalExpense.models import Salary, OtherExpense, DailyExpenses, ConstructionAndRepair
+from django.db.models.functions import ExtractMonth
 
 class Index(LoginRequiredMixin,TemplateView):
     template_name = "index.html"
@@ -38,17 +39,79 @@ class Index(LoginRequiredMixin,TemplateView):
         # Count the number of events for the current year
         events_count_this_year = events_this_year.count()
 
-
-
+        total_expenses = 0
 
         # Getting Total Sales and Expense
         total_sales = EventSale.objects.aggregate(total_sales=Sum('total_amount'))['total_sales']
-        total_expenses = EventExpense.objects.aggregate(total_expenses=Sum('total_expense'))['total_expenses']
+        expenses = EventExpense.objects.aggregate(total_expenses=Sum('total_expense'))['total_expenses']
+        if expenses is None:
+            expenses = 0
+
+        total_expenses += expenses 
 
         total_sales = total_sales or 0
         total_expenses = total_expenses or 0
 
-        
+        # Get the total salary amount for each employee type
+        employee_type_totals = (
+            Salary.objects.values('employee_type')
+            .annotate(total_amount=Sum('amount'))
+        )
+
+        # Get the total salary amount for each month
+        month_totals = (
+            Salary.objects.annotate(month=ExtractMonth('on_date'))
+            .values('month')
+            .annotate(total_amount=Sum('amount'))
+        )
+
+        # Format the results for the report
+        salary_report = []
+        for entry in employee_type_totals:
+            salary_report.append(f"{entry['employee_type']} total is {entry['total_amount']}k")
+
+        for entry in month_totals:
+            month = entry['month']
+            total_amount = entry['total_amount']
+            salary_report.append(f"Salary of the month {month} is {total_amount}k")
+
+        # Print or return the salary_report as needed
+        for line in salary_report:
+            print(line)
+
+
+
+        # Get the total expense amount for each expense type
+        expense_type_totals = (
+            OtherExpense.objects.values('expense_type')
+            .annotate(total_amount=Sum('amount'))
+        )
+
+        # Get the total expense amount for each month
+        month_totals = (
+            OtherExpense.objects.annotate(month=ExtractMonth('on_date'))
+            .values('month')
+            .annotate(total_amount=Sum('amount'))
+        )
+
+        # print(expense_type_totals)
+        # print(month_totals)
+        # Format the results for the report
+        report = []
+        for entry in expense_type_totals:
+            report.append(f"{entry['expense_type']} total is {entry['total_amount']}")
+
+        for entry in month_totals:
+            month = entry['month']
+            total_amount = entry['total_amount']
+            report.append(f"Daily Expenses of the month {month} is {total_amount}k")
+
+        # Print or return the report as needed
+        print("Report is")
+        for line in report:
+            print(line)
+
+
 
         context = {
             "total_sales": total_sales,
