4089d07c7f0336490c07762ef1d50300db3feb12
Habib Ur Rehman <habib.rehman0088@gmail.com>
2023-10-27 18:00:54 +0500

    menu updates error solved

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1556,8 +1556,10 @@ class UpdateFoodMenu(LoginRequiredMixin,TemplateView):
 
             per_head = int(hallCharges) + menuAmount
             eventsale.per_head = per_head
-            eventsale.total_amount = (int(numberOfPeople) * int(per_head)) + (int(extra_charge) + int(stage_charges) + int(entry_charge)) 
-            
+            total = (int(numberOfPeople) * int(per_head)) + (int(extra_charge) + int(stage_charges) + int(entry_charge)) 
+            eventsale.remaining_amount = total - int(eventsale.recieved_amount)
+            eventsale.total_amount = total
+            eventsale.total_menu = subTotal
             eventsale.save()
             
             print(menu)
