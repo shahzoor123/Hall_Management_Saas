5d1fad43530ffe087f4f13fa57fdf82c6c663d27
shahzor khan <shahzoorkhan321@gmail.com>
2023-12-11 03:50:44 +0500

    Pos in custom deal page the grand total bug is fixed

--- morvin/settings/development.py
+++ morvin/settings/development.py
@@ -10,7 +10,7 @@ ALLOWED_HOSTS = ['127.0.0.1','localhost']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db_data',
+        'NAME': 'sk_DB',
     }
 }
 
