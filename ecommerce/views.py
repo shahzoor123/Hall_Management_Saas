34e4c138c52442f1d944534dc66b339fd767299c
Habib Ur Rehman <habib.rehman0088@gmail.com>
2023-12-22 15:45:55 +0500

    expense edit bug fixed

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -909,6 +909,12 @@ class UpdateEventExpense(LoginRequiredMixin, View):
                 decor_bill = int(decor_bill)
                 # print(pakwan, naan_price, drink, bottles, bbq_price, diesel, waiters, stuff, dhobi, other_expenses, setup, decor_bill)
 
+                # print("New ")
+                # print(pakwan, electicity, naan_price, bottles, drink, bbq_price, diesel, waiters, stuff, dhobi, other_expenses, setup, decor_bill )
+
+                # print("OLD")
+                # print(requests.electicity, requests.electicity, requests.naan_bill, bottles, requests.cold_drink_bill, requests.bbq_price, requests.diesel_ltr, requests.waiters_bill, stuff, dhobi, other_expenses, setup, decor_bill )
+
 
                 total = pakwan + electicity + naan_price + bottles + drink + bbq_price + diesel + waiters + stuff + dhobi + other_expenses + setup + decor_bill
 
@@ -933,6 +939,7 @@ class UpdateEventExpense(LoginRequiredMixin, View):
                 requests.other_expense_detals = expense_details
                 requests.setup_bill = setup
                 requests.decor = decor
+                requests.stuff_bill = stuff
                 requests.decor_bill = decor_bill
                 requests.total_expense = total
             
